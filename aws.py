451aefb357a2cb8fa1cce1d1c230938ad8a31f7a
Dallen <dallen.mcnerney@gmail.com>
2019-03-20 22:03:59 -0400

    Created function to convert file paths to S3 standard and incorporated into upload_file function

--- aws.py
+++ aws.py
@@ -2,9 +2,15 @@ import boto3
 import os
 
 
-def upload_file(file, bucket, filename=None):
-    s3 = boto3.resource('s3')
-    if not filename:
-        filename = os.path.split(file)[1]
-    s3.meta.client.upload_file(file, bucket, filename)
+def convert_file_path_s3(file_path):
+    file_path.replace(os.sep, '/')
+    return file_path
 
+
+def upload_file(file, bucket, file_name=None):
+    file = convert_file_path_s3(file)
+    s3 = boto3.resource('s3')
+    if not file_name:
+        file_name = os.path.split(file)[1]
+    file_name = convert_file_path_s3(file_name)
+    s3.meta.client.upload_file(file, bucket, file_name)
