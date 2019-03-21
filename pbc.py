6f748e0d280b0ba5027afd1ce18085a54810d0a0
Dallen <dallen.mcnerney@gmail.com>
2019-03-20 22:13:33 -0400

    Fixed inventory function in pbc to return all files rather than a single file

--- pbc.py
+++ pbc.py
@@ -2,7 +2,9 @@ import os
 
 
 def inventory(parent_directory):
+    file_list = []
     for path, dirs, files in os.walk(parent_directory):
         for file in files:
-            return os.path.join(path, file)
+            file_list.append(os.path.join(path, file))
+    return file_list
 
