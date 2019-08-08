1ada5b694adc583459a95586ffaab6ce0f2974b9
Dallen McNerney <dallen.mcnerney@gmail.com>
2019-08-08 09:55:58 -0400

    Updated sql.py to include functions for reading sql script and identifying tables that are used or created.

--- sql.py
+++ sql.py
@@ -1,5 +1,6 @@
 import pyperclip
 from utils import row_string_to_list
+import re
 
 
 def sql_columns_to_select(select_as=False):
@@ -29,3 +30,27 @@ def trim_sql_columns():
     output_string = output_string[:-2]
     pyperclip.copy(output_string)
     return output_string
+
+
+# TODO: Add functionality for temp tables
+
+
+def identify_used_sql_tables(script_location):
+    with open(script_location, 'r') as sql_file:
+        file_text = sql_file.read()
+        parsed_data = re.findall(r'(from\s+|FROM\s+)(\n*)(dbo\.|DBO\.|\[dbo\]\.|\[DBO\]\.)*(\[*\w+\]*)', file_text)
+        table_list = []
+        for data in parsed_data:
+            table_list.append(data[-1].replace('[', '').replace(']', ''))
+        return table_list
+
+
+def identify_created_sql_tables(script_location):
+    with open(script_location, 'r') as sql_file:
+        file_text = sql_file.read()
+        parsed_data = re.findall(r'(into\s+|INTO\s+)(\n*)(dbo\.|DBO\.|\[dbo\]\.|\[DBO\]\.)*(\[*\w+\]*)', file_text)
+        table_list = []
+        for data in parsed_data:
+            table_list.append(data[-1].replace('[', '').replace(']', ''))
+        return table_list
+
