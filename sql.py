0cb85c779686c802c964f311fc70d9dc3e4e7e31
Dallen McNerney <dallen.mcnerney@gmail.com>
2019-08-08 01:07:58 -0400

    Updated sql.py to include function to generate trim commands around columns

--- sql.py
+++ sql.py
@@ -1,15 +1,31 @@
 import pyperclip
+from utils import row_string_to_list
 
 
-def format_select_sql_columns(select_as=False):
+def sql_columns_to_select(select_as=False):
     columns = pyperclip.paste()
-    columns_list = columns.split('\n')
+    columns_list = row_string_to_list(columns)
+    if columns_list[-1] == '':
+        columns_list.pop(-1)
     output_string = ''
     for column in columns_list:
         if select_as:
             output_string += ''.join(('[', column, '] as [', column, '],\n'))
         else:
             output_string += ''.join(('[', column, '],\n'))
+    output_string = output_string[:-2]
+    pyperclip.copy(output_string)
+    return output_string
+
+
+def trim_sql_columns():
+    columns = pyperclip.paste()
+    columns_list = row_string_to_list(columns)
+    if columns_list[-1] == '':
+        columns_list.pop(-1)
+    output_string = ''
+    for column in columns_list:
+        output_string += ''.join(('LTRIM(RTRIM([', column, '])) as [', column, '],\n'))
     output_string = output_string[:-2]
     pyperclip.copy(output_string)
     return output_string
